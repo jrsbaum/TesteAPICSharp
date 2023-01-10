57fac202bf5205897c5c9754202caef0566ca658
Júnior Baum <juniorsbaum@hotmail.com>
2023-01-10 02:45:11 -0300

    initial commit

---
+++ TesteAPI/Cliente.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TesteAPI
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        [StringLength(20)]
+        public string Nome { get; set; } = string.Empty;
+        [StringLength(20)]
+        public string Email { get; set; } = string.Empty;
+        public string Localizacao { get; set; } = string.Empty;
+        public int Telefone { get; set; }
+        public int Registro { get; set; }
+    }
+}
